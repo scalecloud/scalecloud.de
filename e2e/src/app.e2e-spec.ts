5f99a6136dd34c12e1b21999c9884f9cae2636c6
Sembeth <46921730+Sembeth@users.noreply.github.com>
2021-06-14 18:35:08 +0200

    renamed project

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angular-signup-verification-boilerplate app is running!');
+    expect(page.getTitleText()).toEqual('scalecloud app is running!');
   });
 
   afterEach(async () => {
