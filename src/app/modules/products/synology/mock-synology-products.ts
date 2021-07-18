e3ccb11fe0b6f4c5941010e62cfca05a13b139f3
Sembeth <46921730+Sembeth@users.noreply.github.com>
2021-07-18 15:28:24 +0200

    Changed productIDs and trialDays

--- src/app/modules/products/synology/mock-synology-products.ts
+++ src/app/modules/products/synology/mock-synology-products.ts
@@ -2,35 +2,35 @@ import { SynologyProduct } from "./synology-product";
 
 export const SYNOLOGYPRODUCTS: SynologyProduct[] = [
   {
-    productId: 0,
+    productId: 4,
     name: "Midnight",
     storageAmount: 3,
     scalable: true,
-    trialDays: 30,
+    trialDays: 14,
     usersPerSubscription: 1,
     pricePerMonth: 10
   }, {
-    productId: 1,
+    productId: 5,
     name: "Royal",
     storageAmount: 6,
     scalable: true,
-    trialDays: 30,
+    trialDays: 14,
     usersPerSubscription: 1,
     pricePerMonth: 15
   }, {
-    productId: 2,
+    productId: 6,
     name: "Icy",
     storageAmount: 12,
     scalable: true,
-    trialDays: 30,
+    trialDays: 14,
     usersPerSubscription: 1,
     pricePerMonth: 25
   }, {
-    productId: 3,
+    productId: 7,
     name: "Rose",
     storageAmount: 24,
     scalable: true,
-    trialDays: 30,
+    trialDays: 14,
     usersPerSubscription: 1,
     pricePerMonth: 50
   }
