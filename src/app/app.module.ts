ffb5756bf9a792a2e1c440577ab5610c60634ee4
Sembeth <46921730+Sembeth@users.noreply.github.com>
2021-11-13 00:21:32 +0100

    Added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DefaultModule } from './layouts/default/default.module';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { providePerformance,getPerformance } from '@angular/fire/performance';
 
 @NgModule({
   declarations: [
@@ -14,9 +20,16 @@ import { DefaultModule } from './layouts/default/default.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    DefaultModule
+    DefaultModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    providePerformance(() => getPerformance())
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'scalecloud-de',
+    appId: '1:128016775020:web:bb2b49ad68fb097980e549',
+    storageBucket: 'scalecloud-de.appspot.com',
+    apiKey: 'AIzaSyAjSwXMWnY0nM40nz3pDCAKAvnoG2LG2bw',
+    authDomain: 'scalecloud-de.firebaseapp.com',
+    messagingSenderId: '128016775020',
+    measurementId: 'G-ZRHPQGG8LB',
+  },
   production: true
 };
