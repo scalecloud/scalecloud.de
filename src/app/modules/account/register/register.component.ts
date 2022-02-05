b9f23720c493c1a469ca7f5dcfe32ef3a1b00bec
Sembeth <46921730+Sembeth@users.noreply.github.com>
2022-02-05 17:23:40 +0100

    Created register form

--- src/app/modules/account/register/register.component.ts
+++ src/app/modules/account/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required, Validators.minLength(8), Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})")]);
+  passwordConfirm = new FormControl('', [Validators.required, Validators.minLength(8), Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})")]);
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getErrorMessageEMail() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  getErrorMessagePassword() {
+    var ret = "";
+    if (this.password.hasError('required')) {
+      ret = 'You must enter a value';
+    }
+    else if (this.password.hasError('minlength')) {
+      ret = 'Password must be at least 8 characters long';
+    }
+    else if (this.password.hasError('pattern')) {
+      ret = 'Password must contain at least one uppercase letter, one lowercase letter, one number and one special character';
+    }
+    return ret;
+  }
+
+  getErrorMessagePasswordConfirm() {
+    var ret = "";
+    if (this.passwordConfirm.hasError('required')) {
+      ret = 'You must enter a value';
+    }
+    else if (this.passwordConfirm.hasError('minlength')) {
+      ret = 'Password must be at least 8 characters long';
+    }
+    else if (this.passwordConfirm.hasError('pattern')) {
+      ret = 'Password must contain at least one uppercase letter, one lowercase letter, one number and one special character';
+    }
+    else if (this.password.value !== this.passwordConfirm.value) {
+      ret = 'Passwords did not match';
+    }
+    return ret;
   }
 
 }
