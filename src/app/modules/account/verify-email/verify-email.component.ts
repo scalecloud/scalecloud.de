0c56fe2aeb5a13288b727881253adbe22f51a94b
Sembeth <46921730+Sembeth@users.noreply.github.com>
2022-03-28 19:56:12 +0200

    The button to resend the confirmation E-Mail is disabled for 60 seconds after the page is opened and clicked. The text of the button displays a countdown indicating when it is possible to resend the confirmation E-Mail.

--- src/app/modules/account/verify-email/verify-email.component.ts
+++ src/app/modules/account/verify-email/verify-email.component.ts
@@ -7,10 +7,45 @@ import { AuthService } from 'src/app/shared/services/auth.service';
   styleUrls: ['./verify-email.component.scss']
 })
 export class VerifyEmailComponent implements OnInit {
+  clicked = false;
+  defaultDisabledSecounds = 60;
+  secounds = 0;
 
-  constructor(public authService: AuthService) {}
+  constructor(public authService: AuthService) { }
 
   ngOnInit(): void {
+    this.disableButtonForSeconds();
   }
+
+  sendVerificationMail() {
+    this.authService.sendVerificationMail();
+    this.disableButtonForSeconds();
+  }
+
+  getButtonText(): string {
+    let ret = "";
+    if (this.secounds > 0) {
+      ret = "Resend Verification E-Mail (" + this.secounds + ")";
+    }
+    else {
+      ret = "Resend Verification E-Mail";
+    }
+    return ret;
+  }
+
+  disableButtonForSeconds() {
+    this.clicked = true;
+    this.secounds = this.defaultDisabledSecounds;
+
+    let interval = setInterval(() => {
+      this.secounds--;
+      if (this.secounds < 1) {
+        this.clicked = false;
+        clearInterval(interval);
+      }
+    }, 1000);
+  }
+
+
 
 }
