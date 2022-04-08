00b0ac30fa83eebf1eba6ab349396ebedf142e75
Sembeth <46921730+Sembeth@users.noreply.github.com>
2022-04-08 16:54:27 +0200

    Code smells

--- src/app/modules/main/page-not-found/page-not-found.component.ts
+++ src/app/modules/main/page-not-found/page-not-found.component.ts
@@ -5,11 +5,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.scss']
 })
-export class PageNotFoundComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class PageNotFoundComponent {
 
 }
