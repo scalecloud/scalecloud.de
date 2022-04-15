73c0c43e04bf583550e48ddd1615b133569b9b1a
Sembeth <46921730+Sembeth@users.noreply.github.com>
2022-04-15 23:11:42 +0200

    Added warning that website is currently under construction.

--- src/app/modules/main/home/home.component.ts
+++ src/app/modules/main/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SnackBarService } from 'src/app/shared/services/snackbar/snack-bar.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  constructor(public snackBarService: SnackBarService) { }
+
   ngOnInit(): void {
+    this.snackBarService.errorDuration("This Website is currently under construction. Please come back later.", 30);
   }
+
+
 
 }
