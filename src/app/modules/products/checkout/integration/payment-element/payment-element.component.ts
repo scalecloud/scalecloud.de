debbe06c06575f4865e2822b6e96197724e8b7c5
Sembeth <46921730+Sembeth@users.noreply.github.com>
2022-09-11 16:17:21 +0200

    Started implementing the start of the subscription

--- src/app/modules/products/checkout/integration/payment-element/payment-element.component.ts
+++ src/app/modules/products/checkout/integration/payment-element/payment-element.component.ts
@@ -17,6 +17,8 @@ declare const Stripe: any;
 export class PaymentElementComponent {
 
   checkoutIntegrationReply: CheckoutIntegrationReply | undefined;
+  stripeElement: any;
+  elements: any;
 
   constructor(
     private logService: LogService,
@@ -52,6 +54,8 @@ export class PaymentElementComponent {
     return 10;
   }
 
+
+
   initPaymentElements(): void {
     // Your Stripe public key
     const publicKey = this.stripeKeyService.getPublicKey();
@@ -59,65 +63,59 @@ export class PaymentElementComponent {
       this.logService.error("Cannot display Payment because publicKey is undefined.")
     }
     else {
-      const stripe = Stripe(publicKey);
-      if (this.checkoutIntegrationReply) {
-        const options = {
-          clientSecret: this.checkoutIntegrationReply.clientSecret,
-          // Fully customizable with appearance API.
-          appearance: {/*...*/ },
-        };
-
-        const elements = stripe.elements(options);
-        const paymentElement = elements.create('payment');
-        paymentElement.mount('#payment-element');
-
-        paymentElement.addEventListener('change', (event: { error: { message: string | null; }; }) => {
-          const displayError = document.getElementById('card-errors');
-          if (displayError) {
-            if (event.error) {
-              displayError.textContent = event.error.message;
-              if (displayError.textContent) {
-                this.snackBarService.error(displayError.textContent);
-              }
-            } else {
-              displayError.textContent = '';
-            }
-          }
-        });
-
-        const form = document.getElementById('payment-form');
-        if (form) {
-          form.addEventListener('submit', async (event) => {
-            event.preventDefault();
-
-            const { error } = await stripe.confirmPayment({
-              //`Elements` instance that was used to create the Payment Element
-              elements,
-              confirmParams: {
-                return_url: "https://www.scalecloud.de/order/123/complete",
-              }
-            });
-
-            if (error) {
-              // This point will only be reached if there is an immediate error when
-              // confirming the payment. Show error to your customer (for example, payment
-              // details incomplete)
-              const messageContainer = document.querySelector('#error-message');
-              this.snackBarService.error(error.message);
-            } else {
-              // Your customer will be redirected to your `return_url`. For some payment
-              // methods like iDEAL, your customer will be redirected to an intermediate
-              // site first to authorize the payment, then redirected to the `return_url`.
-            }
-          });
-        }
-      }
+      this.stripeElement = Stripe(publicKey);
     }
+    if (this.checkoutIntegrationReply) {
+      const options = {
+        clientSecret: this.checkoutIntegrationReply.clientSecret,
+        // Fully customizable with appearance API.
+        appearance: {/*...*/ },
+      };
+
+      this.elements = this.stripeElement.elements(options);
+      const paymentElement = this.elements.create('payment');
+      paymentElement.mount('#payment-element');
+
+      paymentElement.addEventListener('change', (event: { error: { message: string | null; }; }) => {
+        const displayError = document.getElementById('card-errors');
+        if (displayError) {
+          if (event.error) {
+            displayError.textContent = event.error.message;
+            if (displayError.textContent) {
+              this.snackBarService.error(displayError.textContent);
+            }
+          } else {
+            displayError.textContent = '';
+          }
+        }
+      });
+    }
+
   }
 
-  startSubscription( quantity: number ): void {
+  startSubscription(quantity: number): void {
     this.logService.info("Checking if quantity was updated and subscriptions needs to be updated: " + quantity);
-   
+    
+    const elements = this.elements
+    const { error } = this.stripeElement.confirmPayment({
+      //`Elements` instance that was used to create the Payment Element
+      elements,
+      confirmParams: {
+        return_url: "https://www.scalecloud.de/order/123/complete",
+      }
+    });
+
+    if (error) {
+      // This point will only be reached if there is an immediate error when
+      // confirming the payment. Show error to your customer (for example, payment
+      // details incomplete)
+      const messageContainer = document.querySelector('#error-message');
+      this.snackBarService.error(error.message);
+    } else {
+      // Your customer will be redirected to your `return_url`. For some payment
+      // methods like iDEAL, your customer will be redirected to an intermediate
+      // site first to authorize the payment, then redirected to the `return_url`.
+    }
   }
 
 }
