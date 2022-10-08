771d8196a2756752dbb7654bfa7b2a56297e6419
Sembeth <46921730+Sembeth@users.noreply.github.com>
2022-10-08 22:16:37 +0200

    Added to status page a status message

--- src/app/modules/products/checkout/status/status.component.ts
+++ src/app/modules/products/checkout/status/status.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AuthService } from 'src/app/shared/services/auth.service';
 import { LogService } from 'src/app/shared/services/log/log.service';
+import { StripeKeyService } from 'src/app/shared/services/stripe/stripe-key.service';
+
+declare const Stripe: any;
 
 @Component({
   selector: 'app-status',
@@ -13,13 +17,17 @@ export class StatusComponent {
   setup_intent_client_secret: string | undefined;
   redirect_status: string | undefined;
 
+  message: string = "Loading...";
+
   constructor(
     private logService: LogService,
     private route: ActivatedRoute,
+    private authService: AuthService,
+    private stripeKeyService: StripeKeyService
   ) { }
 
   ngAfterViewInit(): void {
-    this.initParamMap();
+    this.waitForAuth();
   }
 
   initParamMap(): void {
@@ -41,6 +49,75 @@ export class StatusComponent {
       if (redirect_status) {
         this.redirect_status = redirect_status;
       }
+    }
+  }
+
+  waitForAuth(): void {
+    this.authService.afAuth.authState.subscribe((user) => {
+      if (user) {
+        this.checkPaymentIntentStatus();
+      }
+    });
+  }
+
+  checkPaymentIntentStatus(): void {
+    // Your Stripe public key
+    const publicKey = this.stripeKeyService.getPublicKey();
+    if (publicKey == undefined) {
+      this.logService.error("Cannot display status because publicKey is undefined.")
+    }
+    else {
+      // Initialize Stripe.js using your publishable key
+      const stripe = Stripe(publicKey);
+
+
+      // Retrieve the "payment_intent_client_secret" query parameter appended to
+      // your return_url by Stripe.js
+      this.initParamMap();
+
+      // Check if param is defined
+      if (this.setup_intent_client_secret == undefined) {
+        this.logService.error("Cannot display status because setup_intent_client_secret is undefined.")
+      }
+      else if (this.setup_intent == undefined) {
+        this.logService.error("Cannot display status because setup_intent is undefined.")
+      }
+      else if (this.redirect_status == undefined) {
+        this.logService.error("Cannot display status because redirect_status is undefined.")
+      }
+      else {
+        // Retrieve the PaymentIntent
+        stripe.retrievePaymentIntent(this.setup_intent_client_secret).then(({ paymentIntent }: any) => {
+
+          // Inspect the PaymentIntent `status` to indicate the status of the payment
+          // to your customer.
+          //
+          // Some payment methods will [immediately succeed or fail][0] upon
+          // confirmation, while others will first enter a `processing` state.
+          //
+          // [0]: https://stripe.com/docs/payments/payment-methods#payment-notification
+          switch (paymentIntent.status) {
+            case 'succeeded':
+              this.message = 'Success! Payment received.';
+              break;
+
+            case 'processing':
+              this.message = "Payment processing. We'll update you when payment is received.";
+              break;
+
+            case 'requires_payment_method':
+              this.message = 'Payment failed. Please try another payment method.';
+              // Redirect your user back to your payment page to attempt collecting
+              // payment again
+              break;
+
+            default:
+              this.message = 'Something went wrong.';
+              break;
+          }
+        });
+      }
+
     }
   }
 
