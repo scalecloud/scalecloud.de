3bd3861ff6519ac3057ab6b7923837f9c8bdc1a2
Sembeth <46921730+Sembeth@users.noreply.github.com>
2022-10-08 15:22:06 +0200

    Implemented reading of params after checkout completed

--- src/app/modules/products/checkout/status/status.component.ts
+++ src/app/modules/products/checkout/status/status.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { LogService } from 'src/app/shared/services/log/log.service';
 
 @Component({
   selector: 'app-status',
   templateUrl: './status.component.html',
   styleUrls: ['./status.component.scss']
 })
-export class StatusComponent implements OnInit {
+export class StatusComponent {
 
-  constructor() { }
+  setup_intent: string | undefined;
+  setup_intent_client_secret: string | undefined;
+  redirect_status: string | undefined;
 
-  ngOnInit(): void {
+  constructor(
+    private logService: LogService,
+    private route: ActivatedRoute,
+  ) { }
+
+  ngAfterViewInit(): void {
+    this.initParamMap();
+  }
+
+  initParamMap(): void {
+    const queryParamMap = this.route.snapshot.queryParamMap;
+    if (queryParamMap.has('setup_intent')) {
+      const setup_intent = queryParamMap.get('setup_intent');
+      if (setup_intent) {
+        this.setup_intent = setup_intent;
+      }
+    }
+    if (queryParamMap.has('setup_intent_client_secret')) {
+      const setup_intent_client_secret = queryParamMap.get('setup_intent_client_secret');
+      if (setup_intent_client_secret) {
+        this.setup_intent_client_secret = setup_intent_client_secret;
+      }
+    }
+    if (queryParamMap.has('redirect_status')) {
+      const redirect_status = queryParamMap.get('redirect_status');
+      if (redirect_status) {
+        this.redirect_status = redirect_status;
+      }
+    }
   }
 
 }
