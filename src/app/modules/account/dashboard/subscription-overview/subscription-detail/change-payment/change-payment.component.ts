10f51e642e9266422156f4677571474f338753cc
sembeth <sembeth@live.de>
2023-06-18 19:02:13 +0200

    While changing payment method the subscription details are visible

--- src/app/modules/account/dashboard/subscription-overview/subscription-detail/change-payment/change-payment.component.ts
+++ src/app/modules/account/dashboard/subscription-overview/subscription-detail/change-payment/change-payment.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { ISubscriptionDetail } from '../subscription-detail';
+import { ActivatedRoute } from '@angular/router';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { LogService } from 'src/app/shared/services/log/log.service';
+import { SubscriptionDetailService } from '../subscription-detail.service';
 
 @Component({
   selector: 'app-change-payment',
@@ -6,5 +11,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./change-payment.component.scss']
 })
 export class ChangePaymentComponent {
+
+  subscriptionDetail: ISubscriptionDetail | undefined;
+  
+  constructor(
+    public authService: AuthService,
+    private route: ActivatedRoute,
+    private subscriptionDetailService: SubscriptionDetailService,
+    private logService: LogService
+  ) { }
+
+  ngOnInit(): void {
+    this.waitForAuth();
+  }
+
+  waitForAuth(): void {
+    this.authService.afAuth.authState.subscribe((user) => {
+      if (user) {
+        this.reloadSubscriptionDetail();
+      }
+    }
+    );
+  }
+
+  reloadSubscriptionDetail(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id == null) {
+      this.logService.error('SubscriptionDetailComponent.getSubscriptionDetail: id is null');
+    } else {
+      this.subscriptionDetailService.getSubscriptionDetail(id)
+        .subscribe(subscriptionDetail => this.subscriptionDetail = subscriptionDetail);
+    }
+  }
+
 
 }
