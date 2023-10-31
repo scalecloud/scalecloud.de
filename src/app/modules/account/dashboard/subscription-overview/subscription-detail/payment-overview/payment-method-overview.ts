40b514dd9333836690b5a80562c4982a8fd7098b
sembeth <sembeth@live.de>
2023-10-31 01:03:22 +0100

    Added payment overview for paypal and sepa

--- src/app/modules/account/dashboard/subscription-overview/subscription-detail/payment-overview/payment-method-overview.ts
+++ src/app/modules/account/dashboard/subscription-overview/subscription-detail/payment-overview/payment-method-overview.ts
@@ -1,7 +1,24 @@
-export interface PaymentMethodOverviewReply {
-    type: string;
+export interface PaymentMethodOverviewSEPADebit {
+    bank_code: string;
+    branch: string;
+    country: string;
+    last4: string;
+}
+
+export interface PaymentMethodOverviewCard {
     brand: string;
     last4: string;
     exp_month: number;
     exp_year: number;
+}
+
+export interface PaymentMethodOverviewPayPal {
+    email: string;
+}
+
+export interface PaymentMethodOverviewReply {
+    type: string;
+    card: PaymentMethodOverviewCard;
+    sepa_debit: PaymentMethodOverviewSEPADebit;
+    paypal: PaymentMethodOverviewPayPal;
 }
--- src/app/modules/account/dashboard/subscription-overview/subscription-detail/payment-overview/payment-overview.component.ts
+++ src/app/modules/account/dashboard/subscription-overview/subscription-detail/payment-overview/payment-overview.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/shared/services/auth.service';
-import { LogService } from 'src/app/shared/services/log/log.service';
 import { PaymentMethodOverviewReply } from './payment-method-overview';
 import { PaymentMethodOverviewService } from './payment-method-overview.service';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-payment-overview',
@@ -35,32 +33,96 @@ export class PaymentOverviewComponent {
   }
 
   getPaymentMethod(): string {
-    return this.getBrand() + ' ' + this.getLast4() + ' ' + this.getExpiration();
+    let paymentMethodOverview = '';
+    if (this.subscriptionPaymentMethodReply) {
+      if (this.subscriptionPaymentMethodReply.type === 'card') {
+        paymentMethodOverview = this.getPaymentMethodCard();
+      }
+      else if (this.subscriptionPaymentMethodReply.type === 'sepa_debit') {
+        paymentMethodOverview = this.getPaymentMethodSEPADebit();
+      }
+      else if (this.subscriptionPaymentMethodReply.type === 'paypal') {
+        paymentMethodOverview = this.getPaymentMethodPayPal();
+      }
+    }
+    return paymentMethodOverview;
   }
 
-  getBrand(): string {
+  getPaymentMethodCard(): string {
+    return this.getCardBrand() + ' ' + this.getCardLast4() + ' ' + this.getCardExpiration();
+  }
+
+  getCardBrand(): string {
     let brand = '';
-    if (this.subscriptionPaymentMethodReply) {
-      brand = this.subscriptionPaymentMethodReply.brand;
+    if (this.subscriptionPaymentMethodReply.card.brand) {
+      brand = this.subscriptionPaymentMethodReply.card.brand;
       brand = brand[0].toUpperCase() + brand.slice(1).toLowerCase();
     }
     return brand;
   }
 
-  getLast4(): string {
+  getCardLast4(): string {
     let last4 = '';
     if (this.subscriptionPaymentMethodReply) {
-      last4 = this.subscriptionPaymentMethodReply.last4;
+      last4 = this.subscriptionPaymentMethodReply.card.last4;
     }
     return last4;
   }
 
-  getExpiration(): string {
+  getCardExpiration(): string {
     let expiration = '';
     if (this.subscriptionPaymentMethodReply) {
-      expiration = 'Expires on ' + this.subscriptionPaymentMethodReply.exp_month + '/' + this.subscriptionPaymentMethodReply.exp_year;
+      expiration = 'Expires on ' + this.subscriptionPaymentMethodReply.card.exp_month + '/' + this.subscriptionPaymentMethodReply.card.exp_year;
     }
     return expiration;
+  }
+
+  getPaymentMethodSEPADebit(): string {
+    return this.getSEPADebitBankCode() + ' ' + this.getSEPADebitBranch() + ' ' + this.getSEPADebitCountry() + ' ' + this.getSEPADebitLast4();
+  }
+
+  getSEPADebitBankCode(): string {
+    let bankCode = '';
+    if (this.subscriptionPaymentMethodReply) {
+      bankCode = this.subscriptionPaymentMethodReply.sepa_debit.bank_code;
+    }
+    return bankCode;
+  }
+
+  getSEPADebitBranch(): string {
+    let branch = '';
+    if (this.subscriptionPaymentMethodReply) {
+      branch = this.subscriptionPaymentMethodReply.sepa_debit.branch;
+    }
+    return branch;
+  }
+
+  getSEPADebitCountry(): string {
+    let country = '';
+    if (this.subscriptionPaymentMethodReply) {
+      country = this.subscriptionPaymentMethodReply.sepa_debit.country;
+    }
+    return country;
+  }
+
+  getSEPADebitLast4(): string {
+    let last4 = '';
+    if (this.subscriptionPaymentMethodReply) {
+      last4 = this.subscriptionPaymentMethodReply.sepa_debit.last4;
+    }
+    return last4;
+  }
+
+  getPaymentMethodPayPal(): string {
+    return this.getPayPalEMail();
+  }
+
+  getPayPalEMail(): string {
+    let email = '';
+    if (this.subscriptionPaymentMethodReply) {
+      email = this.subscriptionPaymentMethodReply.paypal.email;
+    }
+    return email;
   }
 
 }
