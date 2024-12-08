55a463fa321dee7d21fb22a5b31b47cf9d043055
sembeth <sembeth@live.de>
2024-12-08 16:42:42 +0100

    If backendError is empty there will be an log message to analyse why its missing.

--- src/app/shared/interceptors/service-error-interceptor.service.ts
+++ src/app/shared/interceptors/service-error-interceptor.service.ts
@@ -2,17 +2,21 @@ import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse
 import { Injectable } from '@angular/core';
 import { Observable, catchError, throwError } from 'rxjs';
 import { SnackBarService } from '../services/snackbar/snack-bar.service';
+import { LogService } from '../services/log/log.service';
 
 @Injectable()
 export class ServiceErrorInterceptorService implements HttpInterceptor {
-  constructor(private snackBarService: SnackBarService) {}
+  constructor(
+    private snackBarService: SnackBarService,
+    private logService: LogService
+  ) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
         let errorMessage = 'An unknown error occurred! Please try again later.';
         let backendError = '';
-        if( error.error.error ) {
+        if (error.error.error) {
           backendError = error.error.error;
         }
         if (error.status) {
@@ -22,10 +26,21 @@ export class ServiceErrorInterceptorService implements HttpInterceptor {
               errorMessage = 'Unable to connect to the server. Please try again later.';
               break;
             case 403:
-              errorMessage = 'You do not have permission to access this resource. Backend: ' + backendError;
+              if (backendError) {
+                errorMessage = 'You do not have permission to access this resource. Backend: ' + backendError;
+              }
+              else {
+                this.logService.error('missing backend error message in 403 error');
+                errorMessage = 'You do not have permission to access this resource.';
+              }
               break;
             case 404:
-              errorMessage = 'The requested resource was not found. Backend: ' + backendError;
+              if (backendError) {
+                errorMessage = 'The requested resource was not found. Backend: ' + backendError;
+              } else {
+                this.logService.error('missing backend error message in 404 error');
+                errorMessage = 'The requested resource was not found.';
+              }
               break;
             case 408:
               errorMessage = 'The request timed out. Please try again later.';
@@ -34,8 +49,13 @@ export class ServiceErrorInterceptorService implements HttpInterceptor {
               errorMessage = 'You have made too many requests. Please wait and try again later.';
               break;
             case 500:
-              errorMessage = 'An internal server error occurred. Please try again later. Backend: ' + backendError;
-               break;
+              if (backendError) {
+                errorMessage = 'An internal server error occurred. Please try again later. Backend: ' + backendError;
+              } else {
+                this.logService.error('missing backend error message in 500 error');
+                errorMessage = 'An internal server error occurred. Please try again later.';
+              }
+              break;
             case 502:
               errorMessage = 'Bad gateway. Please try again later.';
               break;
@@ -43,7 +63,12 @@ export class ServiceErrorInterceptorService implements HttpInterceptor {
               errorMessage = 'The service is currently unavailable. Please try again later.';
               break;
             default:
-              errorMessage = 'An internal server error occurred. Please try again later. Backend: ' + backendError;
+              if (backendError) {
+                errorMessage = 'An internal server error occurred. Please try again later. Backend: ' + backendError;
+              } else {
+                this.logService.error('missing backend error message in default error');
+                errorMessage = 'An internal server error occurred. Please try again later.';
+              }
               break;
           }
         }
