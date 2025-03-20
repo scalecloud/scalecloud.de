207ed786abd74b9f8a60b03a381371899cc89d0f
sembeth <sembeth@live.de>
2025-03-20 19:45:27 +0100

    Sentry has now an production, testing and development environment.

---
+++ src/app/shared/services/environment/environment.ts
@@ -0,0 +1,6 @@
+export enum Environment {
+    Development = 'development',
+    Testing = 'testing',
+    Production = 'production',
+    Unknown = 'unknown',
+}
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,28 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import * as Sentry from "@sentry/angular";
+import * as Sentry from '@sentry/angular';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { EnvironmentService } from './app/shared/services/environment/environment.service';
+
+// Manually create an instance of the EnvironmentService
+const environmentService = new EnvironmentService();
+const sentryEnvironment = environmentService.getEnvironment();
 
 Sentry.init({
-  dsn: "https://37ae26106eaa1531ba2941ee13b103c5@o4508966853083136.ingest.de.sentry.io/4508971996872784",
+  dsn: 'https://37ae26106eaa1531ba2941ee13b103c5@o4508966853083136.ingest.de.sentry.io/4508971996872784',
   integrations: [
     Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration()
+    Sentry.replayIntegration(),
   ],
-  environment: environment.production ? 'production' : 'development',
+  environment: sentryEnvironment,
   // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", /^https:\/\/api\.scalecloud\.de/],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ['localhost', /^https:\/\/api\.scalecloud\.de/],
   // Session Replay
-  replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  replaysSessionSampleRate: 1.0,
+  replaysOnErrorSampleRate: 1.0,
 });
 
 if (environment.production) {
