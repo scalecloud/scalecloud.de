d40e7389c63984f9e8f2df3b9111fec9aad61b1a
sembeth <sembeth@live.de>
2025-03-20 22:57:23 +0100

    Added profiling

--- src/main.ts
+++ src/main.ts
@@ -9,16 +9,15 @@ Sentry.init({
   dsn: "https://37ae26106eaa1531ba2941ee13b103c5@o4508966853083136.ingest.de.sentry.io/4508971996872784",
   integrations: [
     Sentry.browserTracingIntegration(),
+    Sentry.browserProfilingIntegration(),
     Sentry.replayIntegration()
   ],
   environment: environment.production ? 'production' : 'development',
-  // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracesSampleRate: 1.0,
   tracePropagationTargets: ["localhost", /^https:\/\/api\.scalecloud\.de/],
-  // Session Replay
-  replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  replaysSessionSampleRate: 1.0,
+  replaysOnErrorSampleRate: 1.0,
+  profilesSampleRate: 1.0,
 });
 
 if (environment.production) {
