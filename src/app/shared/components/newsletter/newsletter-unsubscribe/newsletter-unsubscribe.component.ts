4e211cee810cec9104e2685f1db44ffe31e22006
sembeth <sembeth@live.de>
2025-03-25 19:01:56 +0100

    Added unsubscribe logik and components.

--- src/app/shared/components/newsletter/newsletter-unsubscribe/newsletter-unsubscribe.component.ts
+++ src/app/shared/components/newsletter/newsletter-unsubscribe/newsletter-unsubscribe.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServiceStatus } from 'src/app/shared/services/service-status';
+import { NewsletterService } from '../newsletter.service';
+import { LogService } from 'src/app/shared/services/log/log.service';
+import { ActivatedRoute } from '@angular/router';
+import { NewsletterUnsubscribeReply, NewsletterUnsubscribeRequest } from '../newsletter';
 
 @Component({
   selector: 'app-newsletter-unsubscribe',
@@ -6,6 +11,46 @@ import { Component } from '@angular/core';
   styleUrl: './newsletter-unsubscribe.component.scss',
   standalone: false,
 })
-export class NewsletterUnsubscribeComponent {
+export class NewsletterUnsubscribeComponent implements OnInit {
+
+  reply: NewsletterUnsubscribeReply | undefined;
+  ServiceStatus = ServiceStatus;
+  serviceStatus = ServiceStatus.Loading;
+
+  constructor(
+    private readonly newsletterService: NewsletterService,
+    private readonly logService: LogService,
+    private readonly route: ActivatedRoute,
+  ) { }
+
+  ngOnInit(): void {
+    this.confirmNewsletter();
+  }
+
+  confirmNewsletter(): void {
+    this.serviceStatus = ServiceStatus.Loading;
+    let request: NewsletterUnsubscribeRequest = {
+      newsletterUUID: this.getNewsletterUUID(),
+    };
+    if (request.newsletterUUID === '') {
+      this.serviceStatus = ServiceStatus.Error;
+      this.logService.error('newsletterUUID is empty current URL: ' + window.location.href);
+      return;
+    }
+    this.newsletterService.unsubscribeFromNewsletter(request)
+      .subscribe({
+        next: reply => {
+          this.reply = reply;
+          this.serviceStatus = ServiceStatus.Success;
+        },
+        error: error => {
+          this.serviceStatus = ServiceStatus.Error;
+        }
+      });
+  }
+
+  getNewsletterUUID(): string {
+    return this.route.snapshot.paramMap.get('newsletterUUID') || '';
+  }
 
 }
