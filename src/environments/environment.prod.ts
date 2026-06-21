e7e5787a2c31b2da04e87b85c843850718bc21b5
sembeth <sembeth@live.de>
2026-06-21 14:15:52 +0200

    Changed firebase prod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
-  firebase: {
-    projectId: 'scalecloud-de',
-    appId: '1:128016775020:web:bb2b49ad68fb097980e549',
-    storageBucket: 'scalecloud-de.appspot.com',
-    locationId: 'europe-west',
-    apiKey: 'AIzaSyAjSwXMWnY0nM40nz3pDCAKAvnoG2LG2bw',
-    authDomain: 'scalecloud-de.firebaseapp.com',
-    messagingSenderId: '128016775020',
-    measurementId: 'G-ZRHPQGG8LB',
+  firebaseConfig: {
+    apiKey: "AIzaSyAjSwXMWnY0nM40nz3pDCAKAvnoG2LG2bw",
+    authDomain: "scalecloud-de.firebaseapp.com",
+    databaseURL: "https://scalecloud-de-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "scalecloud-de",
+    storageBucket: "scalecloud-de.appspot.com",
+    messagingSenderId: "128016775020",
+    appId: "1:128016775020:web:bb2b49ad68fb097980e549",
+    measurementId: "G-ZRHPQGG8LB"
   },
   production: true
 };
