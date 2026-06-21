914ee43f6043edc7fd692a9ed2121d447b66d469
sembeth <sembeth@live.de>
2026-06-21 18:40:08 +0200

    bump to angular 21

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import * as Sentry from "@sentry/angular";
 
@@ -24,5 +24,5 @@ if (environment.production) {
 }
 
 platformBrowserDynamic()
-  .bootstrapModule(AppModule)
+  .bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
   .catch((err) => console.error(err));
