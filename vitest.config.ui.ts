b3a8183bf235bfebb1894f3a1f47ce6ebd585e4d
sembeth <sembeth@live.de>
2026-06-26 20:08:42 +0200

    Rollback to minimal

--- vitest.config.ui.ts
+++ vitest.config.ui.ts
@@ -1,15 +1,6 @@
 import { defineConfig } from 'vitest/config';
-import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
-  resolve: {
-    alias: [
-      {
-        find: /^src\/(.*)$/,
-        replacement: fileURLToPath(new URL('./src/$1', import.meta.url)),
-      },
-    ],
-  },
   test: {
     ui: true,
   },
