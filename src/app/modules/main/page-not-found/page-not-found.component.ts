b343f34fbb7eb9624b2ab7d452ab9f0487cfb259
sembeth <sembeth@live.de>
2026-06-30 21:34:43 +0200

    Extended test

--- src/app/modules/main/page-not-found/page-not-found.component.ts
+++ src/app/modules/main/page-not-found/page-not-found.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, ChangeDetectionStrategy, inject } from '@angular/cor
 import { Router, RouterLink } from '@angular/router';
 import { LogService } from 'src/app/shared/services/log/log.service';
 import { MatCard, MatCardTitle, MatCardContent, MatCardActions } from '@angular/material/card';
-import { MatList } from '@angular/material/list';
 import { MatButton } from '@angular/material/button';
 
 @Component({
@@ -10,7 +9,7 @@ import { MatButton } from '@angular/material/button';
     templateUrl: './page-not-found.component.html',
     styleUrls: ['./page-not-found.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [MatCard, MatCardTitle, MatCardContent, MatList, MatCardActions, MatButton, RouterLink]
+    imports: [MatCard, MatCardTitle, MatCardContent, MatCardActions, MatButton, RouterLink]
 })
 export class PageNotFoundComponent implements OnInit {
   private readonly router = inject(Router);
